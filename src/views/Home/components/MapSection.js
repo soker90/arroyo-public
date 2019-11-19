253e491d993ad5a90283b40b98db64fb47bd01ff
Eduardo Parra <edue17@gmail.com>
2019-11-20 00:03:55 +0100

    map home

--- src/views/Home/components/MapSection.js
+++ src/views/Home/components/MapSection.js
@@ -1,51 +1,55 @@
-import React, {memo} from "react";
+import React, {memo} from 'react';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
 // core components
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
 
-import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
-import mailImage from '../../../assets/img/mail.png';
-import {isWidthUp} from '@material-ui/core/withWidth';
+import styles from 'assets/jss/material-kit-react/views/landingPageSections/productStyle.js';
+import mailImage from 'assets/img/mail.png';
+// import {isWidthUp} from '@material-ui/core/withWidth';
 
 const useStyles = makeStyles(styles);
 
-const MapSection = memo((props) => {
+const MapSection = memo(() => {
   const classes = useStyles();
 
-  const container  =
+  /* const optionsGrid  =
     (isWidthUp('sm', props.width)) ?
-      GridContainer : GridContainer;
+      {xs: 12} : {xs: 6};
+*/
 
   return (
     <div className={classes.section}>
-
-      <GridContainer justify="center">
-        <GridItem xs={12}>
-          <h3 className={classes.subtitle}>Encuéntranos en</h3>
-          <iframe width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"
-                  src="https://maps.google.es/maps?ie=UTF8&amp;q=C%C3%A1rnicas+Arroyo&amp;fb=1&amp;gl=es&amp;hq=carnicas+arroyo&amp;hnear=0xd69aee4af0d7f0f:0x2899244e40b7f09b,Alc%C3%A1zar,+Ciudad+Real&amp;cid=937855447493708146&amp;ll=39.389748,-3.203652&amp;spn=0.006295,0.006295&amp;t=m&amp;output=embed"/>
-        </GridItem>
-        <GridItem xs={12}>
-          <h4 className={classes.textCenter}>
-            Cárnicas Arroyo C.B.<br/>
-            C/Arroyo Mina, 35 Local<br/>
-            Alcázar de San Juan<br/><br />
-            Teléfono: <a href='tel:926546979'>926 54 69 79</a><br/>
-            Correo electrónico:
-            <img src={mailImage}/>
-          </h4>
-        </GridItem>
-      </GridContainer>
+      <h3 className={classes.subtitle}>Encuéntranos en</h3>
+      <div className={classes.section}>
+        <div className={classes.container}>
+          <GridContainer justify="center">
+            <GridItem xs={10} sm={12}>
+              <iframe width="60%" height="auto" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"
+                      src="https://maps.google.es/maps?ie=UTF8&amp;q=C%C3%A1rnicas+Arroyo&amp;fb=1&amp;gl=es&amp;hq=carnicas+arroyo&amp;hnear=0xd69aee4af0d7f0f:0x2899244e40b7f09b,Alc%C3%A1zar,+Ciudad+Real&amp;cid=937855447493708146&amp;ll=39.389748,-3.203652&amp;spn=0.006295,0.006295&amp;t=m&amp;output=embed"/>
+            </GridItem>
+            <GridItem xs={2} sm={12}>
+              <h4 className={classes.textCenter}>
+                Cárnicas Arroyo C.B.<br/>
+                C/Arroyo Mina, 35 Local<br/>
+                Alcázar de San Juan<br/><br/>
+                Teléfono: <a href='tel:926546979'>926 54 69 79</a><br/>
+                Correo electrónico:
+                <img src={mailImage}/>
+              </h4>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div>
     </div>
   );
 });
 
 MapSection.propTypes = {
-  width: PropTypes.numeric.isRequired,
+  width: PropTypes.number.isRequired,
 };
 
 MapSection.displayName = 'MapSection';
