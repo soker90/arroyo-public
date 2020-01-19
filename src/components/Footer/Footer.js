f49c4f3bf83a397c9f5a5f34c19e11f6ddcbeef9
Eduardo Parra <edue17@gmail.com>
2020-01-19 23:28:05 +0100

    f

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,25 @@
-/*eslint-disable*/
-import React from "react";
+import React from 'react';
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // material-ui core components
-import { List, ListItem } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import {List, ListItem} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 
-// @material-ui/icons
-import Favorite from "@material-ui/icons/Favorite";
-
-import styles from "assets/jss/material-kit-react/components/footerStyle.js";
+import styles from 'assets/jss/material-kit-react/components/footerStyle.js';
 import {isWidthUp} from '@material-ui/core/withWidth';
 
 const useStyles = makeStyles(styles);
 
 export default function Footer(props) {
   const classes = useStyles();
-  const { whiteFont } = props;
+  const {whiteFont} = props;
   const footerClasses = classNames({
     [classes.footer]: true,
-    [classes.footerWhiteFont]: whiteFont
+    [classes.footerWhiteFont]: whiteFont,
   });
-  const aClasses = classNames({
-    [classes.a]: true,
-    [classes.footerWhiteFont]: whiteFont
-  });
+
   return (
     <footer className={footerClasses}>
       <div className={classes.container}>
@@ -61,8 +54,8 @@ export default function Footer(props) {
         <div className={classes.right}>
           &copy; {1900 + new Date().getYear()} Cárnicas Arroyo C.B.
         </div>
-        { (isWidthUp('sm', props.width)) &&
-          <div className={classes.block}>
+        {(isWidthUp('sm', props.width)) &&
+        <div className={classes.block}>
           C/Arroyo Mina, 35 Local.
           Alcázar de San Juan<br/>
           Tel: <a href='tel:926546979'>926 54 69 79</a>
@@ -73,5 +66,6 @@ export default function Footer(props) {
 }
 
 Footer.propTypes = {
-  whiteFont: PropTypes.bool
+  whiteFont: PropTypes.bool,
+  width: PropTypes.number,
 };
