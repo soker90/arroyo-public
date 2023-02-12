fd3c25aec85606143e460b633812577cda173d60
Eduardo Parra <edue17@gmail.com>
2023-02-12 13:22:19 +0100

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
-import preact from '@astrojs/preact'
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), preact()]
-})
+  integrations: [tailwind()]
+});
