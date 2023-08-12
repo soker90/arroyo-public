71c4183e6a97c34a45a80d0abfa3c1887255690e
Eduardo Parra <edue17@gmail.com>
2023-08-12 20:39:02 +0200

    Update ContactPage.js

--- src/views/ContactPage/components/ContactPage.js
+++ src/views/ContactPage/components/ContactPage.js
@@ -11,8 +11,6 @@ import Parallax from 'components/Parallax/Parallax';
 import ContactDataSection from './ContactDataSection';
 import imageContact from 'assets/img/contac.jpg';
 import {initGA, PageView} from 'utils/analytics';
-import GridItem from '../../../components/Grid/GridItem';
-import GridContainer from '../../../components/Grid/GridContainer';
 
 const useStyles = makeStyles(styles);
 
@@ -24,13 +22,6 @@ const ContactPage = memo(props => {
     initGA();
     PageView();
   }, [])
-
-  const generateFoxyForm = (d, t) => {
-    let g = d.createElement(t),
-      s = d.getElementsByTagName(t)[0];
-    g.src = 'http://es.foxyform.com/js.php?id=339340&sec_hash=06278a8fc19&width=100%"';
-    s.parentNode.insertBefore(g, s);
-  };
 
   return (
     <div>
@@ -51,15 +42,6 @@ const ContactPage = memo(props => {
         <div className={classes.section}>
           <div className={classes.container}>
             <ContactDataSection/>
-            <GridContainer justify="center">
-              <GridItem cs={12} sm={12} md={8}>
-                <h2 className={classes.title}>Contáctanos</h2>
-                <form id="form">
-                  <a id="foxyform_embed_link_339340" href="http://es.foxyform.com/"/>
-                  {generateFoxyForm(document, 'script')}
-                </form>
-              </GridItem>
-            </GridContainer>
           </div>
         </div>
       </div>
